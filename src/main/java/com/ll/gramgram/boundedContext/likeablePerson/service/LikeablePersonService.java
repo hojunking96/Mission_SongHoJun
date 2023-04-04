5783d75d77ecb0988dbb30335cb88da8737ba5f3
hojunking96 <hojunking96@gmail.com>
2023-04-04 15:45:10 +0900

    feat : 호감 저장소에서 삭제 기능 구현 (Service 단)

--- src/main/java/com/ll/gramgram/boundedContext/likeablePerson/service/LikeablePersonService.java
+++ src/main/java/com/ll/gramgram/boundedContext/likeablePerson/service/LikeablePersonService.java
@@ -21,7 +21,7 @@ public class LikeablePersonService {
 
     @Transactional
     public RsData<LikeablePerson> like(Member member, String username, int attractiveTypeCode) {
-        if ( member.hasConnectedInstaMember() == false ) {
+        if (member.hasConnectedInstaMember() == false) {
             return RsData.of("F-2", "먼저 본인의 인스타그램 아이디를 입력해야 합니다.");
         }
 
@@ -47,5 +47,11 @@ public class LikeablePersonService {
 
     public List<LikeablePerson> findByFromInstaMemberId(Long fromInstaMemberId) {
         return likeablePersonRepository.findByFromInstaMemberId(fromInstaMemberId);
+    }
+
+    @Transactional
+    public RsData<LikeablePerson> delete(int id) {
+        likeablePersonRepository.deleteById(id);
+        return RsData.of("S-1", "선택하신 인스타유저가 호감상대에서 삭제되었습니다.");
     }
 }
