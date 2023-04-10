3fdd651c110d2b088208702a4f654bf9c02f91dd
hojunking96 <hojunking96@gmail.com>
2023-04-10 14:12:01 +0900

    서비스로 이동

--- src/main/java/com/ll/gramgram/boundedContext/likeablePerson/controller/LikeablePersonController.java
+++ src/main/java/com/ll/gramgram/boundedContext/likeablePerson/controller/LikeablePersonController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
-import java.util.Objects;
 
 @Controller
 @RequestMapping("/likeablePerson")
@@ -69,14 +68,8 @@ public class LikeablePersonController {
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/delete/{id}")
     public String delete(@PathVariable Long id) {
-        LikeablePerson likeablePerson = likeablePersonService.findById(id).orElse(null);
+        RsData deleteRs = likeablePersonService.delete(rq.getMember(), id);
 
-        if (likeablePerson == null) return rq.historyBack("이미 취소된 호감입니다.");
-
-        if (!Objects.equals(rq.getMember().getInstaMember().getId(), likeablePerson.getFromInstaMember().getId()))
-            return rq.historyBack("권한이 없습니다.");
-
-        RsData deleteRs = likeablePersonService.delete(likeablePerson);
 
         if (deleteRs.isFail()) return rq.historyBack(deleteRs);
 
