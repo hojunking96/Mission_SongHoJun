d3721decae254457cd614445d305c3bae910921b
hojunking96 <hojunking96@gmail.com>
2023-04-11 16:02:48 +0900

    feat : 호감 목록 검색 기능, 호감사유 수정 기능 구현

--- src/main/java/com/ll/gramgram/boundedContext/likeablePerson/repository/LikeablePersonRepository.java
+++ src/main/java/com/ll/gramgram/boundedContext/likeablePerson/repository/LikeablePersonRepository.java
@@ -2,9 +2,20 @@ package com.ll.gramgram.boundedContext.likeablePerson.repository;
 
 import com.ll.gramgram.boundedContext.likeablePerson.entity.LikeablePerson;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface LikeablePersonRepository extends JpaRepository<LikeablePerson, Long> {
     List<LikeablePerson> findByFromInstaMemberId(Long fromInstaMemberId);
+
+    LikeablePerson findByFromInstaMemberIdAndToInstaMemberId(Long fromInstaMemberId, Long toInstaMemberId);
+
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE likeable_person L SET L.attractive_type_code = :attractiveTypeCode WHERE L.id=:id", nativeQuery = true)
+    void update(@Param("id")Long id, @Param("attractiveTypeCode")int attractiveTypeCode);
 }
