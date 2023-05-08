97e3139ae9f58f3a2dcf3fa75f3c90e9933a3935
hojunking96 <hojunking96@gmail.com>
2023-05-08 09:31:38 +0900

    hasUnreadNotifications

--- src/main/java/com/ll/gramgram/boundedContext/notification/repository/NotificationRepository.java
+++ src/main/java/com/ll/gramgram/boundedContext/notification/repository/NotificationRepository.java
@@ -10,4 +10,5 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
     List<Notification> findByToInstaMember(InstaMember toInstaMember);
 
     List<Notification> findByToInstaMember_username(String username);
+    int countByToInstaMemberAndReadDateIsNull(InstaMember instaMember);
 }
